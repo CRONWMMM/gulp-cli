7c7c73ca26404cd62ae672498a6a82e7b84df01d
CRONWMMM <kimmmiy@sina.cn>
2018-04-12 21:26:14 +0800

    用koa2代替express

--- server.js
+++ server.js
@@ -1,83 +1,32 @@
-/**
- * Created by kimmm on 2018/3/21.
- */
-const port = 8000;
-const http = require('http');
 const path = require('path');
-const express = require('express');
-const debug = require('debug')('mydemo:server');
-const app = express();
-/**
- * Get port from environment and store in Express.
- */
-app.set('port', port);
+const Koa = require('koa');
+const koaViews = require('koa-views');
+const koaStatic = require('koa-static');
+const Router = require('koa-router');
+const app = new Koa();
+const router = new Router();
+const port = 8000;
 
-const server = http.createServer(app);
+// configs
+const viewsPath = './build/views';
+const staticPath = './build';
 
-let dirname = 'build';
+// template render
+app.use(koaViews(path.join(__dirname, viewsPath), {
+    extension: 'html'
+}))
 
-// view engine setup
-app.set('views', path.join(__dirname, dirname+'/views/'));
-app.set('view engine', 'html');
-app.engine('.html',require('ejs').__express);//两个下划线
-// app.use(express.static(path.join(__dirname, dirname+'/static')));
-app.use(express.static(path.join(__dirname, dirname)));
+// static
+app.use(koaStatic(
+    path.join(__dirname, staticPath)
+))
 
-app.get('/', (req, res) => {
-    res.redirect('/index');
-});
+// router
+router.redirect('/', '/index')
+router.get('/index', async (ctx, next) => {
+    await ctx.render('index')
+})
 
-app.get('/index', (req, res) => {
-    res.render('index.html');
-});
-
-app.get('/login', (req, res) => {
-    res.render('login.html');
-});
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-
-    var bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    debug('Listening on ' + bind);
-}
+app.use(router.routes())
+   .use(router.allowedMethods())
+   .listen(port);
